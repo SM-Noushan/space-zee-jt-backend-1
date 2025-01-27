6cc9afa42c1f9d12b04d26dcd5b1331cc6a310c7
SMNoushan <noushan.md@gmail.com>
2025-01-28 04:23:36 +0600

    add: backend2 config

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -6,5 +6,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT || 5000,
   dbUrl: process.env.DB_URL,
+  backendUrl2: process.env.BACKEND_URL2,
+  secretToken: process.env.SECRET_TOKEN,
   bcyptSalRound: process.env.BCRYPT_SALT_ROUNDS || 10,
 };
